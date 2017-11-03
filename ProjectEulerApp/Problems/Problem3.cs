e7e4f5c4b996bfc3f142e25946655384b49db815
Marc Whyte <marcwhyte1987@googlemail.com>
2017-11-03 14:17:52 +0000

    Problems added to ProblemsCollection via Reflection
    GetProblems() now gets all calsses that implement IProblem and puts them in a SortedDictionary, sorted by a custom comparer that orders them by the numeric part of the name.
    
    Also added Problems 3 and 9.

---
+++ ProjectEulerApp/Problems/Problem3.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerApp.Problems
+{
+    class Problem3 : UtilityMethods, IProblem
+    {
+        private string _name = "Problem 3";
+        private string _title = "Largest prime factor";
+        private string _description = "The prime factors of 13195 are 5, 7, 13 and 29."
+            + Environment.NewLine + Environment.NewLine
+            + "What is the largest prime factor of the number 600851475143 ?";
+        string _requiredAnswer = "6857";
+        private BigInteger _myAnswer;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string RequiredAnswer
+        {
+            get { return _requiredAnswer; }
+            set { _requiredAnswer = value; }
+        }
+
+        public BigInteger MyAnswer
+        {
+            get { return _myAnswer; }
+            set { _myAnswer = value; }
+        }
+
+        public BigInteger GetAnswer()
+        {
+            BigInteger number = 600851475143;
+            BigInteger newNum = number;
+            BigInteger answer = 0;
+
+            int counter = 2;
+            while (counter * counter <= newNum)
+            {
+                if (newNum % counter == 0)
+                {
+                    newNum = newNum / counter;
+                    answer = counter;
+                }
+                else
+                {
+                    counter = (counter == 2) ? 3 : counter + 2;
+                }
+            }
+            if (newNum > answer)
+            {
+                answer = newNum;
+            }
+            return answer;
+        }
+    }
+}
--- ProjectEulerApp/ProblemsCollection.cs
+++ ProjectEulerApp/ProblemsCollection.cs
@@ -2,48 +2,53 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace ProjectEulerApp
 {
     class ProblemsCollection
     {
-        public static Dictionary<string, IProblem> GetProblems()
+        public static SortedDictionary<string, IProblem> GetProblems()
         {
-            Dictionary<string, IProblem> problems = new Dictionary<string, IProblem>();
-            Problem1 problem1 = new Problem1();
-            problems.Add(problem1.Name, problem1);
-            Problem2 problem2 = new Problem2();            
-            problems.Add(problem2.Name, problem2);
+            var instances = from t in Assembly.GetExecutingAssembly().GetTypes()
+                            where t.GetInterfaces().Contains(typeof(IProblem))
+                            && t.GetConstructor(Type.EmptyTypes) != null
+                            select Activator.CreateInstance(t) as IProblem;
 
+            ProbComp probComp = new ProbComp();
+            SortedDictionary < string, IProblem> problems = new SortedDictionary<string, IProblem>(new ProbComp());
 
-            Problem4 problem4 = new Problem4();
-            problems.Add(problem4.Name, problem4);
-            Problem5 problem5 = new Problem5();
-            problems.Add(problem5.Name, problem5);
-            Problem6 problem6 = new Problem6();
-            problems.Add(problem6.Name, problem6);
-            Problem7 problem7 = new Problem7();
-            problems.Add(problem7.Name, problem7);
+            foreach (var instance in instances)
+            {
+                problems.Add(instance.Name, instance);
+            }
 
-            Problem25 problem25 = new Problem25();
-            problems.Add(problem25.Name, problem25);
-
-            Problem30 problem30 = new Problem30();
-            problems.Add(problem30.Name, problem30);
-
-            Problem34 problem34 = new Problem34();
-            problems.Add(problem34.Name, problem34);
-
-            Problem36 problem36 = new Problem36();
-            problems.Add(problem36.Name, problem36);
-            Problem37 problem37 = new Problem37();
-            problems.Add(problem37.Name, problem37);
-
-            Problem44 problem44 = new Problem44();
-            problems.Add(problem44.Name, problem44);
             return problems;
+        }
+    }
+
+    class ProbComp : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            Regex rx = new Regex("Problem ");
+            string numX = rx.Replace(x, "");
+            string numY = rx.Replace(y, "");
+
+            int intX;
+            int.TryParse(numX, out intX);
+            int intY;
+            int.TryParse(numY, out intY);
+
+            if (intX.CompareTo(intY) != 0)
+            {
+                return intX.CompareTo(intY);
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
